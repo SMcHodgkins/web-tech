930ac8efb8319ffae1e62ad27f83440beaf82e1b
Scott McHodgkins <scott.mchodgkins@umontana.edu>
2020-01-27 21:10:15 -0700

    Added A2

---
+++ assignment-02/js/script.js
@@ -0,0 +1,4 @@
+console.log("Some of my favorite games...I hate this question. There are too many good games: Apex Legends, Left 4 Dead, Persona 5, RedDead2, AstroBot Rescue Mission, Resident Evil 1/2, Silent Hill 1/2...it goes on forever ")
+console.log("Favorite Websites: Notion, Toggl, Todist, Youtube,  Gumroad")
+console.log("Favorite Arists: Caravaggio, John Sweeny, Cedric Peyravernay, Bierdstadt ")
+//I really can't pick top threes. We should chat about games sometime.
---
+++ assignment-02/readme.md
@@ -0,0 +1,5 @@
+# Assignment 2
+---
+Since I've taken creative coding this wasn't too hard for me. I appreciate all the extra content that you've added to the page. I feel its incredibly helpful for me to explore on my own to find more information.
+
+Here's the thing about the background color. Nah. I technically have the a background color associated with my div. But I'll make it flash an annoying hacker green to fulfill the requirements.
