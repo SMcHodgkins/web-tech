4008d075467e19d5eca8ccecb5e131553f801737
Scott McHodgkins <scott.mchodgkins@umontana.edu>
2020-02-18 19:45:52 -0700

    added hw5

--- assignment-05/js/memory.js
+++ assignment-05/js/memory.js
@@ -2,26 +2,28 @@ var imageTags = ["image1","image2","image3","image4","image5","image6","image7",
 var blank = "images/match/img-01.png";
 var imageArray = new Array();
 
+
+
+
 function printBlanks(){
-  //createImages();
+  createImages();
   for(var i = 0; i < imageTags.length; i++){
     document.getElementById(imageTags[i]).src = blank; //setting the source to blank
   }
 }
 
-/*function createImages(){
-  var images = ["images/match/img-02","images/match/img-03","images/match/img-04","images/match/img-05","images/match/img-06"]
-  var count = [0,0];
-
+function createImages(){
+  var images = ["images/match/img-02.png","images/match/img-03.png","images/match/img-04.png","images/match/img-05.png","images/match/img-06.png"];
+  var count = [0,0,0,0,0];
   while(imageArray.length < 10){
-    var randomNumber = Math.floor(Math.random() * images.length)
+    var randomNumber = Math.floor(Math.random() * images.length);
     if(count[randomNumber]< 2){
       imageArray.push(images[randomNumber])
       count[randomNumber] = count[randomNumber] + 1;
     }
   }
-}*/
+}
 
 function flipImage(number){
-  document.getElementById(imageTags[number]).src = images[number];
+  document.getElementById(imageTags[number]).src = imageArray[number];
 }
--- assignment-05/readme.md
+++ assignment-05/readme.md
@@ -1,3 +1,10 @@
 # Assignment 5
 ---
 I'm starting to wonder if there's an easier way to update the dropdown menu on each of my pages with one click rather than manually inputting a new link on each of them.
+
+I'm having trouble getting two columns to sit next to each other using css.
+
+I accidentally set my flip function to the wrong array.
+
+My createImages function is dying on anything above 4.
+  Because I didn't set enough values in the array tracking the random number.
