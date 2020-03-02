b927097d890bb97ed470e6f1c5f2dcf726f8735d
Scott McHodgkins <46869514+SMcHodgkins@users.noreply.github.com>
2020-03-02 11:11:39 -0700

    a7

--- assignment-07/js/viewfinder.js
+++ assignment-07/js/viewfinder.js
@@ -26,14 +26,19 @@ class viewFinder {
 }
 function initializeArray(){
   var vf1 = new viewFinder("In Search of Rubbish: Cheating the System","./images/vf/img1.jpg","This work is a phantasmagoric, ironic kaleidoscopic grid consisting of a seventeenth-century demonological spell book which is reminiscent of narratives of identity, mortality, loss and love. The work deploys illusions with reflective surfaces as turning the mundane into the fantastical.","Salvador Dalí","June 27, 1587");
-  var vf2 = new viewFinder("After the Relevance","./images/vf/img2.jpg",)
+  var vf2 = new viewFinder("salt, revolutionary, volunteer","./images/vf/img2.jpg","This work is an explicit, representational collected and manipulated images consisting of upended objects and rooms which is reminiscent of San Francisco’s architectural and urban markers.","Rembrandt Harmenszoon van Rijn","July 1, 1694");
+  var vf3 = new viewFinder("power","./images/vf/img3.png","This artwork is a combination of futurism and nostalgia. The methods that it draws upon was actually based off the original The Batman in Dick Sprang's 'Tomorrow Begins' project. It was inspired by a book I found from 1967 called A Book of Imaginary Battles and Elements of Fantasy. It is also a inspiration for an early concept piece that I have in mind but do not want to give away just yet.","Albrecht Durer","May 10, 1877");
+  var vf4 = new viewFinder("Chronicle of a normal criminal","./images/vf/img4.jpg","Groundbreaking is not the word to use for this piece of art. But what is? DeGuzman, a planetary scientist at the Goddard Space Flight Center in Greenbelt, Maryland, worked with a team of 11 other scientists at the space agency, including several 'well-known experts on both Mars and asteroids.'' NASA scientists discovered the part of the atmosphere and melted the section on April 18th. The piece has been named 'Ganymede,'' after the Greek god of the ocean. It was sculpted using platinum, garnet, and hexagonal gems from Houston. The painting now sits on the roof of NASA's Jet Propulsion Laboratory at nearby the California Institute of Technology. Here are more details: ...","M.C. Escher","July 20, 1998");
   viewFinderArray.push(vf1);
+  viewFinderArray.push(vf2);
+  viewFinderArray.push(vf3);
+  viewFinderArray.push(vf4);
 }
 function information(){
   //console.log(vf1.theTitle);
-  document.getElementById('title').innerHTML = viewFinderArray[0].theTitle;
-  document.getElementById('vfImage').src = viewFinderArray[0].theImage;
-  document.getElementById('description').innerHTML = viewFinderArray[0].theDescription;
-  document.getElementById('author').innerHTML = "Author: " + viewFinderArray[0].theAuthor;
-  document.getElementById('year').innerHTML = "Year: " + viewFinderArray[0].theYear;
+  document.getElementById('title').innerHTML = viewFinderArray[3].theTitle;
+  document.getElementById('vfImage').src = viewFinderArray[3].theImage;
+  document.getElementById('description').innerHTML = viewFinderArray[3].theDescription;
+  document.getElementById('author').innerHTML = "Author: " + viewFinderArray[3].theAuthor;
+  document.getElementById('year').innerHTML = "Year: " + viewFinderArray[3].theYear;
 }
